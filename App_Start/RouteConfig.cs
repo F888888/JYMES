a5ccfacdbd2a5112c0653491b3c759a9d485bb74
64547833 <64547833@QQ.COM>
2021-04-11 15:02:56 +0800

    login-1

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace JYMES
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "System", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
